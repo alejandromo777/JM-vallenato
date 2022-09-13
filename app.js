fbbadb37b55b623a89e75fd68104c7e0c862e2ea
alejandro moreno <skasantafeskaska@gmail.com>
2022-09-13 18:19:13 -0500

    pagina terminada con secciones completas faltan los links del footer

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 const mobileIconMenu = document.querySelector('.icon-menu');
 const menuMobile = document.querySelector('.navegation__mobile');
 
-const itemVideos = document.querySelector('#uno');
-const itemFotos = document.querySelector('#dos');
-const itemPrecios = document.querySelector('#tres');
-const itemContactos = document.querySelector('#cuatro');
+const itemHome = document.querySelector('#uno');
+const itemVideos = document.querySelector('#dos');
+const itemFotos = document.querySelector('#tres');
+const itemPrecios = document.querySelector('#cuatro');
+const itemContactos = document.querySelector('#cinco');
+
+const verMasButton = document.querySelector('.images__ver-mas');
+const verMenosButton = document.querySelector('.images__ver-menos');
+const sectionDosFotos = document.querySelector('.images__parte-dos');
 
 
 mobileIconMenu.addEventListener('click', toggleMobileMenu);
+itemHome.addEventListener('click', closeMenuMobile)
 itemVideos.addEventListener('click', closeMenuMobile);
 itemFotos.addEventListener('click', closeMenuMobile);
 itemPrecios.addEventListener('click', closeMenuMobile);
 itemContactos.addEventListener('click', closeMenuMobile);
+verMasButton.addEventListener('click', openFotsDos);
+verMenosButton.addEventListener('click', closeFotosDos);
 
 function toggleMobileMenu() {
-    menuMobile.classList.toggle('inactive');
-    console.log("siii");
- }
+   menuMobile.classList.toggle('inactive');
+   console.log("siii");
+}
  
- function closeMenuMobile() {
-    menuMobile.classList.add('inactive');
+function closeMenuMobile() {
+   menuMobile.classList.add('inactive');
+}
+
+ function openFotsDos() {
+   verMasButton.classList.add('inactive');
+   sectionDosFotos.classList.remove('inactive');
+   verMenosButton.classList.remove('inactive');
  }
+
+function closeFotosDos() {
+   verMenosButton.classList.add('inactive');
+   sectionDosFotos.classList.add('inactive');
+   verMasButton.classList.remove('inactive');
+}
